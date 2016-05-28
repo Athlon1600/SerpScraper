7415d3cf2d960c0f68ae5c4daa21bc21b59ac954
Athlon1600 <Athlon2400@gmail.com>
2016-05-28 14:19:23 -0500

    fixed: https://github.com/Athlon1600/SerpScraper/issues/2

---
+++ src/Captcha/DBCSolver.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace SerpScraper\Captcha;
+
+use SerpScraper\Captcha\CaptchaSolver;
+
+class DBCSolver implements CaptchaSolver {
+
+	private $client;
+	
+	function __construct($username, $password){
+		$this->client = new \DeathByCaptcha_SocketClient($username, $password);
+	}
+	
+	function solve($bytes){
+
+		// let's create a random file somewhere
+		$file = tempnam(sys_get_temp_dir(), 'captcha_');
+		
+		// write bytes to that file
+		file_put_contents($file, $bytes);
+		
+		// dbc has function for decoding files already
+		$response = $this->client->decode($file);
+		
+		// remove temp file
+		unlink($file);
+		
+		return $response['text'];
+	}
+}
+
+?>
