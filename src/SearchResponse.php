a43705cb4469778714f5bc12159b0ff543f23ba0
Athlon1600 <Athlon2400@gmail.com>
2020-05-11 21:01:39 -0500

    Update SearchResponse.php

--- src/SearchResponse.php
+++ src/SearchResponse.php
@@ -2,28 +2,28 @@
 
 namespace SerpScraper;
 
-class SearchResponse {
+class SearchResponse
+{
+    // empty if everything went as expected, otherwise contains error string or 'captcha' if captcha
+    public $error = '';
 
-	// empty if everything went as expected, otherwise contains error string or 'captcha' if captcha
-	public $error = '';
-	
-	// full html source from that page
-	public $html = '';
-	
-	// true or false whether it has a next page
-	public $has_next_page = false;
-	
-	// array starting at pos=1 of search results
-	public $results = array();
-	
-	public function __construct(){
-		// do nothing
-	}
-	
-	public function __toString(){
-		$status = $this->error ? 'error' : 'success';
-		return "SearchResponse Status: {$status}, result count: ".count($this->results);
-	}
+    // full html source from that page
+    public $html = '';
+
+    // true or false whether it has a next page
+    public $has_next_page = false;
+
+    // array starting at pos=1 of search results
+    public $results = array();
+
+    public function __construct()
+    {
+        // do nothing
+    }
+
+    public function __toString()
+    {
+        $status = $this->error ? 'error' : 'success';
+        return "SearchResponse Status: {$status}, result count: " . count($this->results);
+    }
 }
-
-?>
