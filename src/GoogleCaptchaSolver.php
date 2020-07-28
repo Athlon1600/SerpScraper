883bc4366f360be816f0b2ecf96981c282c12c4b
Athlon1600 <Athlon2400@gmail.com>
2020-07-28 09:51:44 -0500

    updated GoogleCaptchaSolver to work with latest php-captcha-solver version 2

--- src/GoogleCaptchaSolver.php
+++ src/GoogleCaptchaSolver.php
@@ -4,57 +4,53 @@ namespace SerpScraper;
 
 use CaptchaSolver\TwoCaptcha;
 use CaptchaSolver\Utils;
-use Curl\Client;
 
 class GoogleCaptchaSolver
 {
     protected $solver;
-    /**
-     * @var Client
-     */
-    private $client;
 
-    public function __construct(Client $client)
+    /** @var Browser */
+    private $browser;
+
+    public function __construct(Browser $browser)
     {
-        $this->client = $client;
+        $this->browser = $browser;
     }
 
     protected function getFormParams($html)
     {
-        $data = array();
-
-        if (preg_match('/"continue"\s*value="(.*?)"/', $html, $matches)) {
-            $data['continue'] = htmlspecialchars_decode($matches[1]);
-        }
-
-        if (preg_match("/name='q'\s*value='([^']+)/is", $html, $matches)) {
-            $data['q'] = $matches[1];
-        }
-
-        return $data;
+        return array(
+            'continue' => htmlspecialchars_decode(Utils::getInputValueByName($html, 'continue')),
+            'q' => Utils::getInputValueByName($html, 'q')
+        );
     }
 
     public function solveUsingTwoCaptcha(SearchResponse $response, $key, $timeout = 90)
     {
-        $utils = new Utils();
-
-        $solver = new TwoCaptcha([
-            'key' => $key
+        $solver = new TwoCaptcha\Client([
+            'key' => $key,
+            'proxy' => $this->browser->getProxy()
         ]);
 
         $curl = $response->getCurlResponse();
 
-        $site_key = $utils->findSiteKey($curl->body);
+        $site_key = Utils::findSiteKey($curl->body);
 
         // not on captcha page
         if ($site_key) {
 
-            $solution = $solver->solveReCaptchaV2($site_key, $curl->info->url, $timeout);
+            $request = new TwoCaptcha\InRequest();
+            $request->key = $key;
+            $request->googlekey = $site_key;
+            $request->pageurl = $curl->info->url;
+            $request->data_s = Utils::findDataSVariable($curl->body);
+
+            $solution = $solver->solveReCaptchaV2($request, $timeout);
 
             $form_data = $this->getFormParams($curl->body);
             $form_data['g-recaptcha-response'] = $solution;
 
-            return $this->client->post('https://www.google.com/sorry/index', $form_data);
+            return $this->browser->post('https://www.google.com/sorry/index', $form_data);
         }
 
         return null;
